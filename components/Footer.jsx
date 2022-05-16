70044ae4fc92510ea9dd59491a2beffd22a5661e
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-05-16 17:15:16 -0300

    Estilização do Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { AiFillInstagram, AiFillFacebook } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>© 2022 Gamer Store - Todos os Direitos Reservados</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiFillFacebook />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
