04cea0983744bc71a2b2434dd6d25995c9f9986f
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-05-16 09:35:36 -0300

    Setup dos produtos

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,22 @@ import { client } from "../Lib/client";
 
 import { Product, FooterBanner, HeadBanner } from "../components";
 
-const Home = ({ products, bannerData }) => {
-  return (
-    <>
-      <HeadBanner headBanner={bannerData.length && bannerData[0]} />
-
-      <div className="products-heading">
-        <h2>Produtos gamers mais vendidos</h2>
-        <p>Melhores Headsets do mercado</p>
-      </div>
-      <div className="products-container">
-        {products?.map((product) => product.name)}
-      </div>
-      <FooterBanner />
-    </>
-  );
-};
+console.log();
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeadBanner headBanner={bannerData.length && bannerData[0]} />
+    <div className="products-heading">
+      <h2>Produtos gamers mais vendidos</h2>
+      <p>Melhores Headsets do mercado</p>
+    </div>
+    <div className="products-container">
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+    <FooterBanner />
+  </div>
+);
 
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
