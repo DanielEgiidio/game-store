125a581c0f72a7ac88f870aa0555a9a5bcd9895b
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-05-17 18:33:39 -0300

    Estilização da pagina de produto individual

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>Game Store</title>
+        <title>Gamer Store</title>
       </Head>
       <header>
         <Navbar />
