d83c5067de079d5b2cdb2408f31acdd56d124d48
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-06-08 14:49:14 -0300

    correção de erros no projeto sanity

--- Lib/client.js
+++ Lib/client.js
@@ -4,7 +4,7 @@ import imageUrlBuilder from "@sanity/image-url";
 export const client = sanityClient({
   projectId: "wcf5o1m1",
   dataset: "production",
-  apiVersion: "2022-05-14",
+  apiVersion: "2022-05-10",
   useCdn: true,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
